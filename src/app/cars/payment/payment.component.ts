2eea3f742d342287c9ad8dc6582de48b54903556
Ignacio Valentin Gomez Simonsohn <nacho2012gomez@gmail.com>
2024-11-07 18:19:20 -0300

    Comienzo con Reactive form en payment

--- src/app/cars/payment/payment.component.ts
+++ src/app/cars/payment/payment.component.ts
@@ -1,15 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-payment',
   templateUrl: './payment.component.html',
   styleUrl: './payment.component.css'
 })
-export class PaymentComponent {
+export class PaymentComponent implements OnInit {
   paymentMethod: 'cash' | 'card' | null=null; 
+  paymentForm : FormGroup
 
   selectPaymentMethod(method: 'cash' | 'card') {
     this.paymentMethod = method;
   }
+  constructor(
+    private fb: FormBuilder,
+  ){
+    this.paymentForm = this.fb.group({
+      fullName : ['Nombre Completo', Validators.required]
+    })
+  }
 
-}
+  ngOnInit(): void {}
+
+} 
