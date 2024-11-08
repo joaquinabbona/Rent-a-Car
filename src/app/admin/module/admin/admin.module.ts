58fc3a408f9609a5907f4f1c0f728d81cc08fa1e
gonzalovazace <gonvazace@gmail.com>
2024-11-08 19:54:05 -0300

    creadas carpetas admin

--- src/app/admin/module/admin/admin.module.ts
+++ src/app/admin/module/admin/admin.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AdminLoginComponent } from '../../components/admin-inicio/admin-inicio.component';
+import { AdminAddComponent } from '../../components/admin-add/admin-add.component';
+import { AdminListComponent } from '../../components/admin-list/admin-list.component';
 
 
 
