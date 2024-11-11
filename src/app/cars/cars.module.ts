8a3e0b8e8b701e18f581b2d633e7ab8ea209134f
JuanUnanue <juanunanue99@gmail.com>
2024-11-11 18:37:39 -0300

    Revert "Matching branches"
    This reverts commit 0a196443f9800ef9cc552c43b3d1c9107ae43001.

--- src/app/cars/cars.module.ts
+++ src/app/cars/cars.module.ts
@@ -2,23 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-<<<<<<< Updated upstream
 import { AddCarsComponent } from './add-cars/add-cars.component';
 import { CarDetailsComponent } from './car-details/car-details.component';
 import { ListCarsComponent } from './list-cars/list-cars.component';
 import { CarManagerComponent } from './car-manager/car-manager.component';
 import { EditCarsComponent } from './edit-cars/edit-cars.component';
 import { PaymentComponent } from './payment/payment.component';
-=======
-import { AddCarsComponent } from './components/add-cars/add-cars.component';
-import { CarDetailsComponent } from './components/car-details/car-details.component';
-import { ListCarsComponent } from './components/list-cars/list-cars.component';
-import { CarManagerComponent } from './components/car-manager/car-manager.component';
-import { EditCarsComponent } from './components/edit-cars/edit-cars.component';
-import { PaymentComponent } from './components/payment/payment.component';
-import { CarsRoutingModule } from './cars-routing.module';
-import { BookingCalendarComponent } from './comppnents/booking-calendar/booking-calendar.component';
->>>>>>> Stashed changes
 
 @NgModule({
   declarations: [
@@ -27,8 +16,7 @@ import { BookingCalendarComponent } from './comppnents/booking-calendar/booking-
     CarDetailsComponent,
     CarManagerComponent,
     EditCarsComponent,
-    PaymentComponent,
-    BookingCalendarComponent
+    PaymentComponent
   ],
   imports: [
     CommonModule,
