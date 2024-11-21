b642181f0a4a69c8b5672bc68035e7569644999f
JuanUnanue <juanunanue99@gmail.com>
2024-11-21 15:25:15 -0300

    Reapply "Change the branch when you suscces the payment"
    This reverts commit ae0a7ca71396bdc9f1ef368fd31852951b5d487a.

--- src/app/cars/services/branch.service.ts
+++ src/app/cars/services/branch.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Branch } from '../models/branch';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -28,4 +29,15 @@ export class BranchService {
   getBranchById(id: number): Observable<Branch> {
     return this.http.get<Branch>(`${this.url}/${id}`);
   }
+  getBranchByName(name: string): Observable<Branch> {
+    return this.http.get<Branch[]>(`${this.url}?city=${name}`).pipe(
+      map(branches => {
+        if (branches.length > 0) {
+          return branches[0]; // Returns the first branch with matching name
+        }
+        throw new Error(`Branch with name ${name} not found`);
+      })
+    );
+  }
+  
 }
